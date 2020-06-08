89ebeeb7fba349916d656038541d75ec95f447a0
Piotr Sperka <piotr@sperka.online>
2020-06-08 20:14:08 +0200

    Angular init

--- politiscales-angular/src/app/app.module.ts
+++ politiscales-angular/src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {QuizComponent} from './quiz/quiz.component';
+import {ResultsComponent} from './results/results.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'quiz', component: QuizComponent},
+  {path: 'results', component: ResultsComponent},
+  {path: '**', component: WelcomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
+    QuizComponent,
+    ResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: false} // <-- debugging purposes only
+    ),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
